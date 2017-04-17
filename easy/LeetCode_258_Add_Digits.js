b4f8eab2271748fac36db20be51d1a3cb296372c
小寶 <gm8300130@gmail.com>
2017-04-17 17:03:52 +0800

    LeetCode 258. Add Digits

---
+++ easy/LeetCode_258_Add_Digits.js
@@ -0,0 +1,47 @@
+/*
+# LeetCode 258. Add Digits
+
+## 題目
+
+Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+
+For example:
+Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+
+Follow up:
+Could you do it without any loop/recursion in O(1) runtime?
+
+## 翻譯
+
+將一個數字每個位數相加，直到剩個位數為止。
+
+範例：
+num = 38，則 3+8 = 11，1+1 = 2, 2是個為數，回傳2。
+
+345 => 3+4+5 = 12 => 1+2 = 3 return 3;
+
+進階：
+不用迴圈，遞迴解這個問題
+ */
+
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var addDigits = function(num) {
+	//代表只有個位數, 可直接跳出
+    if (num < 10) {
+        return num;
+    }
+
+    let str = String(num).split('');
+    let len = str.length - 1;
+    let i = 0 ;
+    //ex : [1, 2, 3] => [1, 3, 6]
+    var array_sum = str.map(function(val){
+        i += parseInt(val);
+    	return i;
+    });
+    //取陣列最後一位
+    return addDigits(array_sum[len]);
+};
