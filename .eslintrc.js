cb40f29b5c7d50d8bb8f7886c271940e5dd1291f
小寶 <gm8300130@gmail.com>
2017-05-08 16:38:06 +0800

    eslint 設定

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "extends": ["standard", "airbnb-base"],
+    "plugins": [
+        "standard",
+        "promise"
+    ],
+    "rules": {
+        "indent": ["error", 4],
+        //字符串必须使用单引号
+        "quotes": [
+            "error",
+            "single"
+        ]
+    }
+};
